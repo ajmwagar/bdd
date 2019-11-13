3be233baec305c7d54622b394b3b0886a1c410dc
Avery Wagar <avery@wagar.cc>
2019-11-12 19:54:28 -0800

    Fixed it

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,75 @@ struct Opts {
     #[structopt(short = "i", long = "input", parse(from_os_str))]
     input: Option<PathBuf>,
 
-    #[structopt(short = "o", long = "output")]
+    #[structopt(short = "o", long = "output", parse(from_os_str))]
     /// Output File(s) to write to. If left empty STDOUT is used.
-    output: Option<Vec<String>>,
+    output: Option<Vec<PathBuf>>,
 
     #[structopt(short = "b", long = "block-size", default_value = "64000")]
     block_size: usize,
 
-    #[structopt(short = "b", long = "block-buffer", default_value = "20")]
-    block_buffer: usize
+    #[structopt(short = "f", long = "block-buffer", default_value = "20")]
+    block_buffer: usize,
+
+    #[structopt(short = "c", long = "count")]
+    block_count: Option<usize>
 }
 
 
 fn main() -> Result<(), std::io::Error> {
     // Grap args
-    let Opts { input, output, block_size, block_buffer } = Opts::from_args();
+    let Opts { input, output, block_size, block_buffer, block_count } = Opts::from_args();
 
-    let mut message_bus = Bus::new(block_buffer);
+    let mut message_bus: Bus<Vec<u8>> = Bus::new(block_buffer);
+
+
+    let writer_threads: Vec<thread::JoinHandle<Result<usize, std::io::Error>>> = match output {
+        Some(outputs) => {
+            outputs.into_iter().map(|output_path| {
+                let mut recv = message_bus.add_rx();
+                thread::spawn(move || {
+                    let mut file = File::create(&output_path)?;
+                    
+                    loop {
+                        match recv.recv() {
+                            Ok(bytes) => {
+                                file.write_all(&bytes)?;
+                            },
+                            Err(_err) => {
+                                file.sync_all()?;
+                                // done writing
+                                break;
+                            }
+                        }
+                    }
+                    
+                    Ok(0)
+                })
+            }).collect()
+        },
+        // Use STDOUT
+        None => {
+            let mut recv = message_bus.add_rx();
+            vec![thread::spawn(move || {
+            let mut stdout = stdout();
+
+            loop {
+                match recv.recv() {
+                    Ok(bytes) => {
+                        stdout.write_all(&bytes)?;
+                    },
+                    Err(err) => {
+                        stdout.flush()?;
+                        // done writing
+                        break;
+                    }
+                }
+            }
+            Ok(0)
+            })]
+            
+        }
+    };
 
     let reader_thread: thread::JoinHandle<Result<usize, std::io::Error>> = thread::spawn(move || {
         match input {
@@ -36,14 +88,34 @@ fn main() -> Result<(), std::io::Error> {
 
                 let mut read = 0;
 
-                while file.stream_position()? < file.stream_len()? {
-                    let mut tmp_buf = vec![0; block_size];
+                match block_count {
+                    Some(count) => {
+                        let mut counter = 0;
+                        while counter < count {
+                            let mut tmp_buf = vec![0; block_size];
+                            read += file.read(&mut tmp_buf)?;
+                            message_bus.broadcast(tmp_buf);
 
-                    read += file.read(&mut tmp_buf)?;
+                            counter += 1;
+                        }
+                    },
+                    None => {
+                        while file.stream_position()? < file.stream_len()? {
+                            let diff = (file.stream_len()? - file.stream_position()?) as usize;
 
-                    message_bus.broadcast(tmp_buf);
 
-                }
+                            let mut tmp_buf = if diff < block_size {
+                                vec![0; diff]
+                            }
+                            else {
+                                vec![0; block_size]
+                            };
+                            read += file.read(&mut tmp_buf)?;
+                            message_bus.broadcast(tmp_buf);
+                        }
+                    }
+                };
+
 
                 Ok(read)
             }
@@ -61,60 +133,14 @@ fn main() -> Result<(), std::io::Error> {
 
     });
 
-    let writer_threads: Vec<thread::JoinHandle<Result<usize, std::io::Error>>> = match output {
-        Some(outputs) => {
-            outputs.iter().map(|output_path| {
-                let mut recv = message_bus.add_rx();
-                thread::spawn(move || {
-                    let mut file = File::open(&output_path)?;
-                    
-                    loop {
-                        match recv.recv() {
-                            Ok(bytes) => {
-                                file.write(&bytes)?;
-                            },
-                            Err(_err) => {
-                                // done writing
-                                break;
-                            }
-                        }
-                    }
-                    
-                    Ok(0)
-                })
-            }).collect()
-        },
-        // Use STDOUT
-        None => {
-            vec![thread::spawn(move || {
-            let mut recv = message_bus.add_rx();
-            let mut stdout = stdout();
-
-            loop {
-                match recv.recv() {
-                    Ok(bytes) => {
-                        stdout.write(&bytes)?;
-                    },
-                    Err(err) => {
-                        // done writing
-                        break;
-                    }
-                }
-            }
-            Ok(0)
-            })]
-            
-        }
-    };
-
     // Wait on threads
     let bytes_read = reader_thread.join().unwrap()?;
 
-    println!("Read {} bytes.", bytes_read);
+    println!("{} bytes copied", bytes_read);
 
-    writer_threads.iter().for_each(|handle| {
-        handle.join();
-    });
+    for handle in writer_threads {
+        handle.join().unwrap()?;
+    }
 
     Ok(())
 }
